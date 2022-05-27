a4c1e7ed06e6a255de2f188530e3015b8d1c7cf2
Derssa <youssefmoinou@gmail.com>
2022-05-27 13:02:39 +0100

    fix deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_APP_SANITY_PROJECT_ID: process.env.NEXT_APP_SANITY_PROJECT_ID,
+    NEXT_APP_SANITY_TOKEN: process.env.NEXT_APP_SANITY_TOKEN,
+    NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
+  },
   images: {
     domains: ["i.ytimg.com"],
   },
