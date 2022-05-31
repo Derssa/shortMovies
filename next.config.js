cbec6446283e8187431cf91440705f446b09959f
Derssa <youssefmoinou@gmail.com>
2022-05-31 13:58:19 +0100

    setup fb

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     NEXT_APP_SANITY_PROJECT_ID: process.env.NEXT_APP_SANITY_PROJECT_ID,
     NEXT_APP_SANITY_TOKEN: process.env.NEXT_APP_SANITY_TOKEN,
     NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
+    NEXT_PUBLIC_FB_PIXEL: process.env.NEXT_PUBLIC_FB_PIXEL,
   },
   images: {
     domains: ["i.ytimg.com"],
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import "../styles/globals.css";
 import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((RP) => {
+        RP.init(`${process.env.NEXT_PUBLIC_FB_PIXEL}`);
+        RP.pageView();
+
+        router.events.on("routeChangeComplete", () => {
+          RP.pageView();
+        });
+      });
+    return () => {
+      router.events.off("routeChangeComplete", () => {
+        RP.pageView();
+      });
+    };
+  }, [router.events]);
+
   return (
     <>
       <Script
